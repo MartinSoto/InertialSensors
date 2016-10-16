4c123aae802484357a60ef49f723d77ccb430c1b
Martin Soto <donsoto@gmail.com>
2016-10-16 15:11:19 +0200

    Initial project setup.
    Webpack (with dev server), Babel ES6.

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var webpack = require("webpack");
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: [
+    './src/main.js'
+  ],
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'inline-source-map',
+
+  plugins: [
+    new CopyWebpackPlugin([
+      { context: 'src', from: '*.html' }
+    ])
+  ]
+};
