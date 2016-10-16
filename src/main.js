714464013be363a4c58e466ec388fe4eab837d6b
Martin Soto <donsoto@gmail.com>
2016-10-16 19:28:22 +0200

    Implement basic data history accumulation.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import { Observable } from 'rx';
 const R = require('ramda');
 
+const accumulateHistory = R.curry((maxValues, previousHist, value) => {
+  const trimmed = previousHist.length > maxValues ? R.drop(1, previousHist) : previousHist;
+  return R.append(value, trimmed);
+});
+
 const main = () => {
   Observable.fromEvent(window, 'devicemotion')
     .map(R.prop('accelerationIncludingGravity'))
-    .forEach(R.partial(console.log, ["Accel:"]));
+    .scan(accumulateHistory(20), [])
+    .forEach(R.partial(console.log, ["Accels:"]));
 };
 
 main();
